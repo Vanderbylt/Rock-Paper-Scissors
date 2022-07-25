5c65f76b69f1c651da91d2ff48989a5bb1bd17b9
Vanderbylt <debashishdas465@gmail.com>
2022-07-25 11:54:35 +0530

    create game for 5 rounds

--- rps.js
+++ rps.js
@@ -11,10 +11,8 @@ function getComputerChoice(){
     }
 }
 
-const computerSelection = getComputerChoice();
-
-let getPlayerChoice = prompt("What's your choice ? Rock, Paper or Scissors");
-let playerSelection = typeof getPlayerChoice === 'string' ? `${getPlayerChoice[0].toUpperCase() + getPlayerChoice.slice(1)}` : "null"; 
+let playerScore = 0,
+computerScore = 0;
 
 
 function playRound(computerSelection,playerSelection){
@@ -31,11 +29,13 @@ function playRound(computerSelection,playerSelection){
         (computerSelection === 'Paper' && playerSelection === 'Rock') ||
         (computerSelection === 'Scissors' && playerSelection === 'Paper')){
             console.log(`You Lose!, ${computerSelection} beats ${playerSelection}.`);
+            computerScore += 1;
         }
         else if ((computerSelection === "Paper" && playerSelection === "Scissors") ||
         (computerSelection === 'Scissors' && playerSelection === 'Rock') ||
         (computerSelection === 'Rock' && playerSelection === 'Paper')){
             console.log(`You Win!, ${playerSelection} beats ${computerSelection}.`) ;
+            playerScore += 1;
         } 
         else{
             console.log("Sorry that's not a choice.");
@@ -43,8 +43,28 @@ function playRound(computerSelection,playerSelection){
     }   
 }
 
-playRound(computerSelection,playerSelection);
+function game(){
+  
+        for (let i = 0; i < 5;i++){
+            var computerSelection = getComputerChoice();
 
+            let getPlayerChoice = prompt("What's your choice ? Rock, Paper or Scissors");
+            let playerSelection = typeof getPlayerChoice === 'string' ? `${getPlayerChoice[0].toUpperCase() + getPlayerChoice.slice(1)}` : "null"; 
+            playRound(computerSelection,playerSelection);
+        }
+
+        if (playerScore<computerScore){
+            console.log("You Lose!, Computer wins.");
+        }
+        else if (computerScore<playerScore){
+            console.log("You Win!, Computer loses.");
+        }
+        else{
+            console.log("Close Call!, It's a Tie.");
+        }
+    }
+
+game();
 
 
 
