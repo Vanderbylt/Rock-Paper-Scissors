908fe3d3439ce6a5a50ceea719ebaf16c5ceae97
Vanderbylt <debashishdas465@gmail.com>
2022-08-12 00:06:14 +0530

    Rename pics to enhance js improve js file more

--- rps.js
+++ rps.js
@@ -11,60 +11,76 @@ function getComputerChoice(){
     }
 }
 
-let playerScore = 0,
-computerScore = 0;
+
+
+
+
 
 
 function playRound(computerSelection,playerSelection){
-    if (playerSelection==='null'){
-        console.log("You Cancelled.");
+    if (computerSelection === playerSelection){
+        text.textContent = 'Oops!, it\'s a tie.';
+        img1.src = `rps-pics\\${playerSelection}inv.png`;
+ 
+        img2.src = `rps-pics\\${computerSelection}.png`;
+        choice1.appendChild(img1);
+        choice2.appendChild(img2);
+    }
+    else if((computerSelection === 'Paper' && playerSelection === 'Rock') ||
+    (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
+    (computerSelection === 'Rock' && playerSelection === 'Scissors')){
+        text.textContent = `You Lose!, ${computerSelection} beats ${playerSelection}.`;
+        cScore+=1;
+        computerScore.textContent = cScore;
+ 
+        img1.src = `rps-pics\\${playerSelection}inv.png`;
+  
+        img2.src = `rps-pics\\${computerSelection}.png`;
+        choice1.appendChild(img1);
+        choice2.appendChild(img2);
+    
+    }
+    else if ((computerSelection === "Paper" && playerSelection === "Scissors") ||
+    (computerSelection === 'Scissors' && playerSelection === 'Rock') ||
+    (computerSelection === 'Rock' && playerSelection === 'Paper')){
+        text.textContent = `You Win!, ${playerSelection} beats ${computerSelection}.`;
+        pScore+=1;
+        playerScore.textContent = pScore;
+
+        img1.src = `rps-pics\\${playerSelection}inv.png`;
+ 
+        img2.src = `rps-pics\\${computerSelection}.png`;
+        choice1.appendChild(img1);
+        choice2.appendChild(img2);
+    } 
+    else {
+        console.log('sorry thats not an option');
+        console.log(computerSelection);
+        console.log(playerSelection);
     }
     
-    else{
-        if (computerSelection===playerSelection){
-            console.log("Oops! its a tie.");
-        }
+}   
 
-        else if ((computerSelection === "Rock" && playerSelection === "Scissors") ||
-        (computerSelection === 'Paper' && playerSelection === 'Rock') ||
-        (computerSelection === 'Scissors' && playerSelection === 'Paper')){
-            console.log(`You Lose!, ${computerSelection} beats ${playerSelection}.`);
-            computerScore += 1;
-        }
-        else if ((computerSelection === "Paper" && playerSelection === "Scissors") ||
-        (computerSelection === 'Scissors' && playerSelection === 'Rock') ||
-        (computerSelection === 'Rock' && playerSelection === 'Paper')){
-            console.log(`You Win!, ${playerSelection} beats ${computerSelection}.`) ;
-            playerScore += 1;
-        } 
-        else{
-            console.log("Sorry that's not a choice.");
-        }
-    }   
-}
+let pScore = 0;
+let cScore = 0;
+let main = document.querySelector('.main');
 
-function game(){
-  
-        for (let i = 0; i < 5;i++){
-            var computerSelection = getComputerChoice();
+const foot = document.querySelector('footer');
+const text = document.getElementById('text');
+const playerScore = document.querySelector('.scores.one');
+const computerScore = document.querySelector('.scores.two');
+const scores = document.querySelectorAll('.scores');
+const buttons = document.querySelectorAll('button');
+const choice1 = document.querySelector('.choice.one');
+const choice2 = document.querySelector('.choice.two');
+const pAgain = document.querySelector('button');
+var img1 = document.createElement('img');
+var img2 = document.createElement('img');
+scores.forEach((score) => {score.classList.add('scores-js')});
 
-            let getPlayerChoice = prompt("What's your choice ? Rock, Paper or Scissors");
-            let playerSelection = typeof getPlayerChoice === 'string' ? `${getPlayerChoice[0].toUpperCase() + getPlayerChoice.slice(1)}` : "null"; 
-            playRound(computerSelection,playerSelection);
-        }
+buttons.forEach((button) => {button.addEventListener('click',() => {playRound(getComputerChoice(),button.id)})} );
 
-        if (playerScore<computerScore){
-            console.log("You Lose!, Computer wins.");
-        }
-        else if (computerScore<playerScore){
-            console.log("You Win!, Computer loses.");
-        }
-        else{
-            console.log("Close Call!, It's a Tie.");
-        }
-    }
 
-game();
 
 
 
